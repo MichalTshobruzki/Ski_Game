3ec6627e0c32b3fed561b5421263616343e7e4fa
MichalTshobruzki <michal.tsho@gmail.com>
2019-03-11 10:04:24 +0200

    .

--- src/game/arenas/ExtremeSkiingArena.java
+++ src/game/arenas/ExtremeSkiingArena.java
@@ -1,5 +1,11 @@
 package game.arenas;
 
+import game.sportsmen.Skier;
+
+import java.util.ArrayList;
+
+import utilities.Point;
+
 /**
  * Extreme Skiing Arena
  * Coefficient of friction - const 0.4
@@ -8,4 +14,24 @@ package game.arenas;
  */
 
 public class ExtremeSkiingArena {
+    private ArrayList <Skier> Skiers = new ArrayList<Skier>();
+    //ArrayList <SunshineSkier> SunshineSkiers = new ArrayList<SunshineSkier>();
+    //ArrayList <Snowboarder> Snowboarders = new ArrayList<Snowboarder>();
+    public Point start;
+    private Point finish;
+    private ArrayList <Object> finished = new ArrayList<>();
+    private static double friction = 0.4;
+    private int maxRaces = 6;
+    private String surface;
+    private String condition;
+    private String discipline;
+
+    private boolean add(Skier newCompetitor){
+        if (Skiers.size()<6){
+            if(newCompetitor instanceof Skier){
+                Skiers.add(newCompetitor);
+            }
+            //else if(newCompetitor instanceof SunshineSkiers)
+        }
+    }
 }
--- src/game/sportsmen/Skier.java
+++ src/game/sportsmen/Skier.java
@@ -7,20 +7,34 @@ import utilities.Point;
  * acceleration- 7
  */
 
-
 public class Skier {
     private String name;
     private Point currentLocation;
-    private double maxSpeed;
-    private double acceleration;
+    private double maxSpeed = 80;
+    private double acceleration = 7;
     private double currentSpeed;
     private String qualification;
     private String gender;
     private double age;
     private String league;
 
+    /**
+     *
+     * @param start
+     */
     public void initRace(Point start){
-
+      //  this.currentLocation = new Point()
+        this.currentLocation.setX(start.getX());
+        this.currentLocation.setY(0);
     }
 
+    public boolean move(Point finish, double friction){
+        if (this.currentSpeed < this.maxSpeed){
+            this.currentSpeed += this.acceleration * getFriction();
+        }
+        this.currentLocation.setX(this.currentLocation.getX() + this.currentSpeed);
+        if (this.currentLocation.getX() >= getFinish())
+            return false;
+        return true;
+    }
 }
